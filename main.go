e33d0673116a2da612f5e01650b18dcb20352a97
Mcpll <mbonini@develer.com>
2024-12-12 11:59:33 +0100

    Fix: Change post review

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"crypto/sha1"
 	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
+	"net/url"
 	"strconv"
 )
 
@@ -24,34 +25,28 @@ func shortenURL(url string) string {
 func handleGenerateShortUrl(db *ShortUrls, port string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		// Here the url should be passed as a JSON object, for example:
-		// curl -X POST -d '{"url": "http://www.google.it"}' http://localhost:8080/create
-		// rather than via form data.
+		var u struct {
+			Url string `json:"url"`
+		}
 
-		err := r.ParseForm()
+		err := json.NewDecoder(r.Body).Decode(&u)
 		if err != nil {
-			// Doing log.Fatal means that any client can crash the server by
-			// just sending a malformed request. You should log the error, send
-			// a reasoannable http status to the client and return to end the
-			// request.
-			log.Fatal("Errore:", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
 
-		url := r.PostForm.Get("url")
-		if url == "" {
-			// same here.
-			log.Fatal("Il valore del parametro url non è corretto o è vuoto")
+		if u.Url == "" {
+			http.Error(w, "Il valore del parametro url non è corretto o è vuoto", http.StatusBadRequest)
+			return
 		}
 
-		// before proceeding with storing the URL, we should check the URL is
-		// valid, for example with:
-		//
-		// _, err := url.Parse(url)
-		//
-		// and return the appropriate status code if the URL is not valid.
-		shortUrl := shortenURL(url)
+		if _,err := url.Parse(u.Url); err != nil {
+			http.Error(w,err.Error(),http.StatusBadRequest)
+		}
 
-		db.urls[shortUrl] = url
+		shortUrl := shortenURL(u.Url)
+
+		db.urls[shortUrl] = u.Url
 
 		fmt.Fprintf(w, "http://localhost%v/short/%v\n", port, shortUrl)
 
@@ -61,13 +56,7 @@ func handleGenerateShortUrl(db *ShortUrls, port string) http.HandlerFunc {
 func handleRedirect(db *ShortUrls) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		shortcode := r.PathValue("shortcode")
-
-		// shortUrl := strings.SplitAfter(r.URL.Path, "/short/")[1]
-		// if shortUrl == "" {
-		// 	// same here about the use of log.Fatal
-		// 	log.Fatal("Short url non presente")
-		// }
+		shortcode := r.PathValue("code")
 
 		http.Redirect(w, r, db.urls[shortcode], http.StatusMovedPermanently)
 	}
@@ -84,12 +73,6 @@ func main() {
 	PORT := ":" + strconv.Itoa(portNum)
 
 	http.HandleFunc("POST /create", handleGenerateShortUrl(&db, PORT))
-	// Here you could use "/short/{code}", this has the advantage of only
-	// entering in the handler if code is non-empty.
-	//
-	// If you do this, then in the handler you can directly use:
-	//  r.PathValue("code")
-	// to access the value of 'code'.
 	http.HandleFunc("/short/{code}", handleRedirect(&db))
 
 	fmt.Println("server start on ", PORT)
